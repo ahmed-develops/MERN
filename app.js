21e720eaa75401e2960e603e0e9d17fc0cd276bf
Muhammad Ahmed <60579788+ahmed-develops@users.noreply.github.com>
2023-06-12 22:30:00 +0500

    Created >1 pages to test the routing feature.

--- app.js
+++ app.js
@@ -2,7 +2,23 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World, from home')
+});
+
+app.get('/about', (req, res) => {
+    res.send('Hello World, from about')
+});
+
+app.get('/login', (req, res) => {
+    res.send('Hello World, from login')
+});
+
+app.get('/contact', (req, res) => {
+    res.send('Hello World, from contact')
+});
+
+app.get('/signup', (req, res) => {
+    res.send('Hello World, from signup')
 });
 
 app.listen(3000, () => {
