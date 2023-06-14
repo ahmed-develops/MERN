4750ff9dd46334a08daa02a7de21ad3bef7ca293
Muhammad Ahmed <60579788+ahmed-develops@users.noreply.github.com>
2023-06-14 21:51:58 +0500

    Added async-await functionality which is another method to save data just like Promise functionality

--- server/router/auth.js
+++ server/router/auth.js
@@ -3,11 +3,11 @@ const User = require('../model/userSchema')
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     res.send('Hello World, from home')
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', async (req, res) => {
 
     // ECMA SCRIPT 2016 CONVENTION OF DISPLAYING DATA
     const {name, email, phone, work, password, confirm_password} = req.body;
@@ -18,24 +18,45 @@ router.post('/register', (req, res) => {
         return res.status(422).json( { error: "Error 422: Unprocessable Content"})
     }
 
-    // validation check upon email
-    User.findOne( {email : email} )
-    .then((userExists) => {
+    // Async version
+    try {
+        const userExists = await User.findOne( {email : email} );
+
         if (userExists) {
             return res.status(422).json( { error: "Error 422: Unprocessable Content"})
         }
 
         const user = new User({name:name, email:email, phone:phone, work:work, password:password, confirm_password:confirm_password});
 
-        // returns promises while registring successfully or unsuccesfully
-        user.save().then(()=> {
-            // successfully saved
-            return res.status(201).json( { msg: "Data saved successfully"})
-        // unsucessful save
-        }).catch((err) => {
-            return res.status(500).json({ error: "Data could not be registered! "})
-        })
-    }).catch(err => { console.log(err) } )
+        await user.save();
+
+        res.status(201).json( { msg: "Data saved successfully"})
+    }
+    catch(err) {
+        console.log(err);
+    }
+
+    // Promise version
+
+    // // validation check upon email
+    // User.findOne( {email : email} )
+    // .then((userExists) => {
+    //     if (userExists) {
+    //         return res.status(422).json( { error: "Error 422: Unprocessable Content"})
+    //     }
+
+    //     const user = new User({name:name, email:email, phone:phone, work:work, password:password, confirm_password:confirm_password});
+
+    //     res.status(201).json( { msg: "Data saved successfully"})
+    //     // returns promises while registring successfully or unsuccesfully
+    //     user.save().then(()=> {
+    //         // successfully saved
+    //         return res.status(201).json( { msg: "Data saved successfully"})
+    //     // unsucessful save
+    //     }).catch((err) => {
+    //         return res.status(500).json({ error: "Data could not be registered! "})
+    //     })
+    // }).catch(err => { console.log(err) } )
 
 
     //res.send("Testing123");
