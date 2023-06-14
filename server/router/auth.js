21ad88ffa51683f770a10322170edb002009fea3
Muhammad Ahmed <60579788+ahmed-develops@users.noreply.github.com>
2023-06-14 19:32:28 +0500

    Added data entry through user.save function and also validation check has been added. Error handling and promises handling done through arrow function.

--- server/router/auth.js
+++ server/router/auth.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const User = require('../model/userSchema')
 
 const router = express.Router();
 
@@ -10,7 +11,33 @@ router.post('/register', (req, res) => {
 
     // ECMA SCRIPT 2016 CONVENTION OF DISPLAYING DATA
     const {name, email, phone, work, password, confirm_password} = req.body;
-    console.log(name);
+    //console.log(name);
+
+    // required fields
+    if (!name || !email || password || confirm_password) {
+        return res.status(422).json( { error: "Error 422: Unprocessable Content"})
+    }
+
+    // validation check upon email
+    User.findOne( {email : email} )
+    .then((userExists) => {
+        if (userExists) {
+            return res.status(422).json( { error: "Error 422: Unprocessable Content"})
+        }
+
+        const user = new User({name:name, email:email, phone:phone, work:work, password:password, confirm_password:confirm_password});
+
+        // returns promises while registring successfully or unsuccesfully
+        user.save().then(()=> {
+            // successfully saved
+            return res.status(201).json( { msg: "Data saved successfully"})
+        // unsucessful save
+        }).catch((err) => {
+            return res.status(500).json({ error: "Data could not be registered! "})
+        })
+    }).catch(err => { console.log(err) } )
+
+
     //res.send("Testing123");
 });
 
